1af9a5509f4682ba0763b948ee3b1b62826acd00
Vinicius-Justino <justinovinicius2006@gmail.com>
2025-09-22 11:34:27 -0300

    Exercício "Brigadeiros" melhorado

--- 2024/python/brigadeiros.py
+++ 2024/python/brigadeiros.py
@@ -1,12 +1,18 @@
 quantidade_pratos, quantidade_amigos, limite_tempo = [int(n) for n in input().split(" ")]
-
 mesa = [int(n) for n in input().split(" ")]
-lugares_vazios = [mesa.index(lugar) for lugar in reversed(sorted(mesa))]
+copia_mesa = mesa.copy()
+
+lugares_vazios = []
+for prato in reversed(sorted(mesa)):
+    lugar = copia_mesa.index(prato)
+    lugares_vazios.append(lugar)
+    copia_mesa[lugar] = -1
+
 lugares_ocupados = [lugares_vazios.pop(0) for i in range(quantidade_amigos)]
 cont_troca_permanente = quantidade_pratos - quantidade_amigos
 
 posicao_inicial = [int(n) for n in input().split(" ")]
-    
+
 while True:
     posicao_ideal = [(1 if i in lugares_ocupados else 0) for i in range(quantidade_pratos)]
     resultado_xor = [(1 if [posicao_inicial[i], posicao_ideal[i]].count(1) == 1 else 0) for i in range(quantidade_pratos)]
@@ -22,14 +28,16 @@ while True:
     if distancia <= limite_tempo:
         break
     
-    if cont_troca_permanente > 0:
-        lugares_vazios.append(lugares_ocupados.pop(-1))
-        cont_troca_permanente -= 1
-    else:
-        lugares_ocupados.pop(-2)
-        lugares_ocupados.insert(0, lugares_vazios.pop(0))
-        cont_troca_permanente = len(lugares_vazios) - 1
+    lugares_vazios.append(lugares_ocupados.pop(-1))
 
+    if cont_troca_permanente == 0:
+        lugares_ocupados.pop(-1)
+        lugares_ocupados.insert(0, lugares_vazios.pop(0))
+        cont_troca_permanente = len(lugares_vazios)   
+        
+    
     lugares_ocupados.append(lugares_vazios.pop(0))
+    cont_troca_permanente -= 1
 
 print(sum([mesa[i] for i in lugares_ocupados]))
+print(lugares_ocupados)
